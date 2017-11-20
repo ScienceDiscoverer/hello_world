b53b76bee3e38bf4619ad53e38890934f2c907d9
ScienceDiscoverer <ScienceDiscoverer@gmail.com>
2017-11-20 17:58:56 +0200

    Added multiplication and some exceptions
    Float 0, very small numbers, infinity

--- Float_RAW_Viewer.cpp
+++ Float_RAW_Viewer.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
 #include <bitset>
 #include <cmath>
+// Sorry for the ugly code :D
 
 using namespace std;
 
 int main()
 {
     //float x = 9.234f;
-    while(1)
+    //setting float to infinity
+	bitset<32> float_inf;
+	float_inf[31] = 1;
+	for(int i = 30; i > 22; --i)
+		float_inf[i] = 1;
+		
+	for(int i = 22; i >= 0; --i)
+		float_inf[i] = 0;
+		
+	cout << "Inf float raw: " << float_inf << endl;
+	unsigned long ul = float_inf.to_ulong();
+	cout << "Inf as int: " << ul << endl;
+	unsigned long* ulp = &ul;
+	float* fp = (float*)ulp;
+	
+	cout << "Inf float: " << *fp << endl;
+	cout << "<--------------------------------------------------->" << endl;
+	//////////////////////////////////////
+	
+	
+	while(1)
     {
-		float x;
-		cin >> x;
+		cout << "Enter 2 float numbers to multiply: ";
+		float x, y;
+		cin >> x >> y;
+		x *= y;
 		float* p = &x;
 	    int* p_i = (int*)p;
 		bitset<32> float_int(*p_i);
@@ -37,12 +60,25 @@ int main()
 			mantissa[i] = float_int[i];
 		}
 		
-		float mant = 1.0f + (float)mantissa.to_ulong() / pow(2, 23);
-		cout << " = " << mant << endl;
+		float mant = (expon == -127 || expon == 128? 0.0f:1.0f) + (float)mantissa.to_ulong() / pow(2, 23);
+		cout << " = " << mant;
+		if(expon == -127 && !mant) cout << " (-127 exp. & 0 mant. = 0)";
+		else if(expon == -127) cout << " (-127 exp. & non-0 mant. = denormalised number)";
+		else if(expon == 128 && !mant) cout << " (128 exp. & 0 mant. = infinity)";
+		cout << endl;
 		
-		cout << "Proof: " << mant << " * " << pow(2.0f, (float)expon) << " = ";
-		if(float_int[31]) cout << '-';
-		cout << mant * pow(2.0f, (float)expon) << endl;
+		cout << "Proof: ";
+		if(expon == 128 && !mant)
+		{
+			if(float_int[31]) cout << '-';
+			cout << 1.0e+30f * 1.0e+38f << endl;
+		}
+		else
+		{
+			cout << mant << " * " << pow(2.0f, (float)expon) << " = ";
+			if(float_int[31]) cout << '-';
+			cout << mant * pow(2.0f, (float)expon) << endl;
+		}
 		
 		cout << "<--------------------------------------------------->" << endl;
 	}
